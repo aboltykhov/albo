31c8b0a5b9dfe1d3f08e85410467416a2fecfc76
Alexey Boltykhov <aboltykhov@mail.ru>
2021-03-06 14:31:24 +0000

    Скрипты итоговой работы для серверов https://websa.advancedhosting.com/

--- httpd-setup.sh
+++ httpd-setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#Предварительно скрипт проверяет возможность своей работы от пользователя
+if [[ $UID -ne 0 ]]; then
+echo "Скрипт требуется запустить в привилегированном режиме sudo или от пользователя root"
+exit 1; fi
+
 #Создать файл ssh ключей для github.com в автозапуске
 cat <<EOF > /etc/ssh/ssh_config.d/github.com.conf
 Host github.com
@@ -13,20 +18,18 @@ chmod 600 /etc/ssh/ssh_config.d/github.com.conf
 #Ключ -y отвечает Да на установку
 sudo yum -y install httpd && sudo systemctl enable httpd && sudo systemctl start httpd
 
-#Подготовка git
-mkdir dz_web_server
-cd dz_web_server
-git init
+#Подготовка git репозитория
+cd /tmp/ && mkdir dz_web_server && cd dz_web_server
 
 #Добавляем нужный репозиторий
-git remote add origin git@github.com:aboltykhov/dz_web_server.git
+git init && git remote add origin git@github.com:aboltykhov/dz_web_server.git
 
 #Скачать бекап из репозитория,
 #Если не скачивает, проверить название ветки, в моем случае main
-git pull origin main
+git pull origin main && ls -lh
 
 #Создаем папки
-mkdir /var/www/8080 /var/www/8081 /var/www/8082
+#mkdir /var/www/8080 /var/www/8081 /var/www/8082
 
 #Разворачиваем бекап
 #Ключ -a копировать содежимое с атрибутами
@@ -39,6 +42,8 @@ cp -a web.bak/etc/httpd/conf.d/8081.conf /etc/httpd/conf.d/
 cp -a web.bak/etc/httpd/conf.d/8082.conf /etc/httpd/conf.d/
 cp -a web.bak/etc/httpd/conf/httpd.* /etc/httpd/conf/
 
+#Показать ip-адреса хоста
+hostname -I
+
 #Перечитать конфигурацию и показать статус
 systemctl restart httpd && systemctl status httpd
-
