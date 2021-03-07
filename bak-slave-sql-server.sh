07f00f4c02d9538c4d8679e35ca7e298e6578659
Alexey Boltykhov <aboltykhov@mail.ru>
2021-03-07 13:51:30 +0000

    Скрипты итоговой работы для серверов https://websa.advancedhosting.com/

---
+++ bak-slave-sql-server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#1)
+#cd /tmp
+#yum -y install wget
+#wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+#rpm -ivh epel-release-latest-8.noarch.rpm
+#yum -y install sshpass
+
+
+#2)
+#Снять бекап cо СЛЕЙВА 10.0.0.2 и отправить на сервер бекапов 10.0.0.3
+sudo mysqldump -u root --password=User1589$ --all-databases --events --routines --master-data=1 > /tmp/"backupDB-"`date +"%Y-%m-%d"`.sql && sshpass -p Candyshop919 scp /tmp/backupDB-*.sql adminroot@10.0.0.3:/tmp
