c62d29d2704d0834592e62052863a2902b34494c
Alexey Boltykhov <aboltykhov@mail.ru>
2021-03-07 12:47:49 +0000

    Скрипты итоговой работы для серверов https://websa.advancedhosting.com/

--- httpd-setup.sh
+++ httpd-setup.sh
@@ -34,14 +34,14 @@ mkdir /var/www/8080 /var/www/8081 /var/www/8082
 
 #Разворачиваем бекап
 #Ключ -a копировать содежимое с атрибутами
-cp -a web.bak/var/www/8080/index.html /var/www/8080/
-cp -a web.bak/var/www/8081/index.html /var/www/8081/
-cp -a web.bak/var/www/8082/index.html /var/www/8082/
+cp -a /tmp/dz_web_server/web.bak/var/www/8080/index.html /var/www/8080/
+cp -a /tmp/dz_web_server/web.bak/var/www/8081/index.html /var/www/8081/
+cp -a /tmp/dz_web_server/web.bak/var/www/8082/index.html /var/www/8082/
 
-cp -a web.bak/etc/httpd/conf.d/8080.conf /etc/httpd/conf.d/
-cp -a web.bak/etc/httpd/conf.d/8081.conf /etc/httpd/conf.d/
-cp -a web.bak/etc/httpd/conf.d/8082.conf /etc/httpd/conf.d/
-cp -a web.bak/etc/httpd/conf/httpd.* /etc/httpd/conf/
+cp -a /tmp/dz_web_server/web.bak/etc/httpd/conf.d/8080.conf /etc/httpd/conf.d/
+cp -a /tmp/dz_web_server/web.bak/etc/httpd/conf.d/8081.conf /etc/httpd/conf.d/
+cp -a /tmp/dz_web_server/web.bak/etc/httpd/conf.d/8082.conf /etc/httpd/conf.d/
+cp -a /tmp/dz_web_server/web.bak/etc/httpd/conf/httpd.* /etc/httpd/conf/
 
 #Показать ip-адреса хоста
 echo && hostname -I && echo 
