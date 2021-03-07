dfdd0758e9583a9e1af5486b1b26cd5b99eb52a5
Alexey Boltykhov <aboltykhov@mail.ru>
2021-03-07 15:43:32 +0000

    Скрипты итоговой работы для серверов https://websa.advancedhosting.com/

--- docker-nginx-setup.sh
+++ docker-nginx-setup.sh
@@ -39,7 +39,7 @@ docker pull aboltykhov/albo-nginx:nginx && docker run -d -p 80:80 aboltykhov/alb
 #################################################################
 rm -rf /tmp/dockerp
 #Установить MySQL слейв, для репликации БД
-cd /tmp/dz_itog
-./new-sql-server-slave.sh
+#cd /tmp/dz_itog
+#./new-sql-server-slave.sh
 
 
