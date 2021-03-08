df82b9e5101644faa3c1c2f052ae90780ba45c69
Alexey Boltykhov <aboltykhov@mail.ru>
2021-03-08 10:37:52 +0000

    Скрипты итоговой работы для серверов https://websa.advancedhosting.com/

--- httpd-setup.sh
+++ httpd-setup.sh
@@ -5,6 +5,7 @@ echo "Скрипт требуется запустить в привилегир
 exit 1; fi
 
 #Создать файл ssh ключей для github.com в автозапуске
+rm -rf /etc/ssh/ssh_config.d/github.com.conf
 cat <<EOF > /etc/ssh/ssh_config.d/github.com.conf
 Host github.com
     HostName github.com
@@ -13,26 +14,25 @@ EOF
 
 #Права на файл ключей ssh
 chmod 600 /etc/ssh/ssh_config.d/github.com.conf
+echo && echo && ssh -T git@github.com
 
 #Устанавка и запуск апача
 #Ключ -y отвечает Да на установку
 yum -y install httpd && sudo systemctl enable httpd && sudo systemctl start httpd
 
-#Подготовка git репозитория
-cd /tmp/ && mkdir dz_web_server && cd dz_web_server
-
 #Установить git и добавляем репозиторий ВЕБ-сервера
+#Подготовка git репозитория
 yum -y install git
 git config --global user.name "Alexey Boltykhov"
 git config --global user.email aboltykhov@mail.ru
 git config --global core.editor vi
 git config pull.rebase false
-git init && git remote add origin git@github.com:aboltykhov/dz_web_server.git
-git config pull.rebase false
+cd /tmp/ && mkdir dz_web_server && cd dz_web_server
 
 #Скачать бекап веб-сервера из репозитория,
 #Если не скачивает, проверить название ветки, в моем случае main
-git pull origin main && ls -lh
+git init && git remote add origin git@github.com:aboltykhov/dz_web_server.git
+git pull origin main && ls -lh && ls -lh /tmp/
 
 #Создаем папки
 rm -rf  /var/www/8080 /var/www/8081 /var/www/8082
@@ -43,10 +43,12 @@ mkdir /var/www/8080 /var/www/8081 /var/www/8082
 cp -a /tmp/dz_web_server/web.bak/var/www/8080/* /var/www/8080/
 cp -a /tmp/dz_web_server/web.bak/var/www/8081/* /var/www/8081/
 cp -a /tmp/dz_web_server/web.bak/var/www/8082/* /var/www/8082/
+cp -a /tmp/dz_web_server/web.bak/var/www/html/* /var/www/html/
 
 cp -a /tmp/dz_web_server/web.bak/etc/httpd/conf.d/8080.conf /etc/httpd/conf.d/
 cp -a /tmp/dz_web_server/web.bak/etc/httpd/conf.d/8081.conf /etc/httpd/conf.d/
 cp -a /tmp/dz_web_server/web.bak/etc/httpd/conf.d/8082.conf /etc/httpd/conf.d/
+rm -rf /etc/httpd/conf/httpd.conf
 cp -a /tmp/dz_web_server/web.bak/etc/httpd/conf/httpd.* /etc/httpd/conf/
 
 #Показать ip-адреса хоста
@@ -64,39 +66,11 @@ sudo chown -R apache:apache /var/www/html/*
 cd /var/www/html/
 cp wp-config-sample.php wp-config.php
 
-#Создать скрипт PHP, который будет подключаться к mysql и запрашивать содержимое
-cat <<EOF > /var/www/html/albo.php
-<?php
-$user = "wpuser";
-$password = "WP1password$";
-$database = "wordpress";
-$table = "list";
-
-try {
-  $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
-  echo "<h2>TODO</h2><ol>";
-  foreach($db->query("SELECT content FROM $table") as $row) {
-    echo "<li>" . $row['content'] . "</li>";
-  }
-  echo "</ol>";
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
-EOF
-
-#Создать проверочный скрипт PHP
-cat <<EOF > /var/www/html/info.php
-<?php
-
-phpinfo();
-EOF
-
 #Перечитать конфигурацию и показать статус
 systemctl restart httpd && systemctl status httpd
 
 #Установить MySQL 
-cd /tmp/dz_itog
-./new-sql-server-master.sh
+cd /tmp/dz_web_server/
+./selinux-off.sh
 
 
