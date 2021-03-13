e4144d10b3ae388094e23bc58b8f184df38f556b
Alexey Boltykhov <aboltykhov@mail.ru>
2021-03-13 13:51:58 +0000

    Скрипты итоговой работы для серверов https://websa.advancedhosting.com/

--- wp-albo.sql
+++ wp-albo.sql
@@ -1,5 +1,7 @@
-USE wordpress;
-CREATE TABLE wordpress.list ( item_id INT AUTO_INCREMENT, content VARCHAR(255), PRIMARY KEY(item_id) );
-INSERT INTO wordpress.list (content) VALUES ("Test Al Bo item 001");
-SELECT * FROM  wordpress.list;
+USE wpdb;
+CREATE TABLE wpdb.albo ( item_id INT AUTO_INCREMENT, content VARCHAR(255), PRIMARY KEY(item_id) );
+INSERT INTO wpdb.albo (content) VALUES ("Al Bo item first");
+INSERT INTO wpdb.albo (content) VALUES ("Al Bo item second");
+INSERT INTO wpdb.albo (content) VALUES ("Al Bo item third ");
+SELECT * FROM  wpdb.albo;
 SHOW GRANTS FOR 'wpuser'@'localhost';
--- wp-db-user.sql
+++ wp-db-user.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE wordpress;
+CREATE DATABASE wpdb;
 CREATE USER 'wpuser'@'localhost' IDENTIFIED BY 'WP1password$';
-GRANT ALL ON wordpress.* TO 'wpuser'@'localhost' WITH GRANT OPTION;
+GRANT ALL ON wpdb.* TO 'wpuser'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 SHOW GRANTS FOR 'wpuser'@'localhost';
 SHOW DATABASES;
