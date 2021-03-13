bea68aa951e70d11fa41b2d0915fca6306aa40d5
Alexey Boltykhov <aboltykhov@mail.ru>
2021-03-13 17:02:39 +0000

    Скрипты итоговой работы для серверов https://websa.advancedhosting.com/

--- new-sql-server-master.sh
+++ new-sql-server-master.sh
@@ -27,6 +27,7 @@ sudo mysql_secure_installation
 #сделаем копию и отредактируем файл конфигра
 cp /etc/my.cnf.d/mysql-server.cnf /etc/my.cnf.d/mysql-server.cnf.bak
 rm -rf /etc/my.cnf.d/mysql-server.cnf
+
 #Добавить строки, ip нового мастера
 cat <<EOF > /etc/my.cnf.d/mysql-server.cnf
 [mysqld]
@@ -34,7 +35,7 @@ datadir=/var/lib/mysql
 socket=/var/lib/mysql/mysql.sock
 log-error=/var/log/mysql/mysqld.log
 pid-file=/run/mysqld/mysqld.pid
-bind-address=10.0.0.3
+bind-address=10.0.0.1
 server-id=1
 EOF
 
@@ -43,13 +44,18 @@ systemctl restart mysqld  && systemctl status mysqld
 
 
 #4)
+#Скачиваем репозиторий dz_itog
+cd /tmp/ && mkdir dz_itog && cd dz_itog
+git init && git remote add origin git@github.com:aboltykhov/dz_itog.git
+git pull origin main && ls -lh && ls -lh /tmp/
+
 #НЕОБЯЗАТЕЛЬНО
 #копируем бекап на новый МАСТЕР
 #требуется знать пользователя и адрес сервера с бекапом
 #ключ scp -r копирует папку целиком 
 #вводим пароль пользователя adminroot
 #scp -r adminroot@10.0.0.3:/tmp/backupDB-*.sql /tmp/
-#sshpass -p Candyshop919 scp -r adminroot@10.0.0.1:/tmp/backupDB-*.sql /tmp/
+#sshpass -p <adminroot password> scp -r adminroot@10.0.0.1:/tmp/backupDB-*.sql /tmp/
 
 #на новом МАСТЕРЕ разворачиваем бекап
 #sudo mysql -u root --password=User1589$ < /tmp/backupDB-*.sql
@@ -62,15 +68,18 @@ systemctl restart mysqld  && systemctl status mysqld
 #но удобнее импортировать из файла *.sql
 sudo mysql -u root --password=User1589$ < /tmp/dz_itog/replication.sql
 
-#Создать тестовую БД, пользователя и предоставить пользователю права на созданную БД
+#Создать тестовую БД и тестового пользователя и предоставить пользователю права на созданную БД
 sudo mysql -u root --password=User1589$ < /tmp/dz_itog/db001.sql
 
 #Создать БД, пользователя для управления базой CMS WordPress и применить изменения
 sudo mysql -u root --password=User1589$ < /tmp/dz_itog/wp-db-user.sql
 
-#Создать таблицу от имени пользователя wpuser
+#Создать таблицу от имени пользователя wpuser, для проверки
 sudo mysql -u wpuser --password=WP1password$ < /tmp/dz_itog/wp-albo.sql
-	
+
+#Следующий скрипт прометей
+
+#################################################################	
 #ПРИМЕРЫ
 #Grant user permissions to all tables in my_database from localhost --
 #GRANT ALL ON my_database.* TO 'user'@'localhost';
@@ -85,8 +94,8 @@ sudo mysql -u wpuser --password=WP1password$ < /tmp/dz_itog/wp-albo.sql
 #################################################################
 #Удалить пользователя
 #SELECT user,host FROM mysql.user;
+#SHOW GRANTS FOR 'wpuser'@'localhost';
 #REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'wpuser'@'localhost';
 #DROP USER 'wpuser'@'localhost';
-#SHOW GRANTS FOR 'wpuser'@'localhost';
 #SELECT user,host FROM mysql.user;
 #################################################################
