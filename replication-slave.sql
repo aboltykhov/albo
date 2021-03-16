16fc09a74aa70eee4cf572ff09119e68ce58ec0c
Alexey Boltykhov <aboltykhov@mail.ru>
2021-03-16 14:38:23 +0000

    Скрипты итоговой работы для серверов https://websa.advancedhosting.com/

--- replication-slave.sql
+++ replication-slave.sql
@@ -1,4 +1,4 @@
 CHANGE MASTER TO MASTER_HOST='10.0.0.1', MASTER_USER='abrepl', MASTER_PASSWORD='User1589Rep$', MASTER_LOG_FILE='binlog.000002', MASTER_LOG_POS=706, GET_MASTER_PUBLIC_KEY = 1; 
 START SLAVE; 
 SHOW SLAVE STATUS\G; 
-show variables like '%read_only%';"
+show variables like '%read_only%';
