944a3768ca5c9df2628592709a497e4a461961aa
Alexey Boltykhov <aboltykhov@mail.ru>
2021-03-17 17:45:40 +0000

    Скрипты итоговой работы для серверов https://websa.advancedhosting.com/

--- replace-master-on-slave.sh
+++ replace-master-on-slave.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #меняем мастера для репликации (для пользователя abrepl)
 #указываем binlog.000000 и MASTER_LOG_POS=000000 нового мастера
-sudo mysql -u root --password=User1589$ < /tmp/dz_itog/change-sql-binlog.sql
+sudo mysql -u root --password=User1589$ < /tmp/albo/SQL/change-sql-binlog.sql
