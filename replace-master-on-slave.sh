37fdc238b77838ff90c860554634511e5e26dee1
Alexey Boltykhov <aboltykhov@mail.ru>
2021-03-18 09:45:11 +0000

    Скрипты итоговой работы для серверов https://websa.advancedhosting.com/

--- replace-master-on-slave.sh
+++ replace-master-on-slave.sh
@@ -4,7 +4,7 @@ if [[ $UID -ne 0 ]]; then
 echo "Скрипт требуется запустить в привилегированном режиме sudo или от пользователя root"
 exit 1; fi
 
-#Меняем мастера для репликации пользователь abrepl
-#Указываем binlog.000000 и MASTER_LOG_POS=000000 нового мастера в change-sql-binlog.sql
+#Меняем мастера для репликации
+#Указываем binlog.000000 и MASTER_LOG_POS=000000 нового мастера в slq/change-sql-binlog.sql
 mysql -u root --password=User1589$ < /tmp/albo/change-sql-binlog.sql
 
